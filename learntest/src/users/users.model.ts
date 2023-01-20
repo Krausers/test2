85aca827ef191e0723bfc2efb2643e2bee960a1e
Данил <SemenchenkovD@yandex.ru>
2023-01-20 03:15:58 +0300

    add db

---
+++ learntest/src/users/users.model.ts
@@ -0,0 +1,12 @@
+import { Model, Table } from "sequelize-typescript";
+
+@Table({tableName: 'users'})
+export class User extends Model<User, > {
+  id: number;
+
+  email: string;
+
+  password: string;
+
+  banned: boolean;
+}
